bd6149177405f05ac9c9d93215b79fa9c01259b1
Jari Mäkelä <jari.makela@albertineapp.com>
2022-02-21 21:07:26 +0200

    Get rid of semicolons

--- src/finnkino.test.ts
+++ src/finnkino.test.ts
@@ -1,8 +1,8 @@
-import { readFileSync } from "fs";
-import { parseFinnkino } from "./finnkino";
+import { readFileSync } from "fs"
+import { parseFinnkino } from "./finnkino"
 
 describe("finnkino", () => {
     it("can parse test xml", () => {
-        parseFinnkino(readFileSync("./test-data/schedule.xml").toString());
-    });
-});
+        parseFinnkino(readFileSync("./test-data/schedule.xml").toString())
+    })
+})
--- src/finnkino.ts
+++ src/finnkino.ts
@@ -1,16 +1,14 @@
 /* eslint-disable no-underscore-dangle */
-import { xml2js } from "xml-js";
-import * as z from "zod";
+import { xml2js } from "xml-js"
+import * as z from "zod"
 
-const TextNode = z
-    .object({ _text: z.string() })
-    .transform((node) => node._text);
+const TextNode = z.object({ _text: z.string() }).transform((node) => node._text)
 const NumberNode = z
     .object({ _text: z.number() })
-    .transform((node) => node._text);
+    .transform((node) => node._text)
 const DateNode = z
     .object({ _text: z.string().transform((s) => new Date(s)) })
-    .transform((node) => node._text);
+    .transform((node) => node._text)
 
 const FinnkinoShow = z.object({
     ID: NumberNode,
@@ -26,9 +24,9 @@ const FinnkinoShow = z.object({
     PresentationMethodAndLanguage: TextNode,
     ShowURL: TextNode,
     EventURL: TextNode,
-});
+})
 
-type FinnkinoShow = z.infer<typeof FinnkinoShow>;
+type FinnkinoShow = z.infer<typeof FinnkinoShow>
 
 export function parseFinnkino(xml: string): FinnkinoShow[] {
     const parsedXml = xml2js(xml, {
@@ -38,10 +36,10 @@ export function parseFinnkino(xml: string): FinnkinoShow[] {
         ignoreAttributes: true,
         ignoreDeclaration: true,
         ignoreDoctype: true,
-    }) as { Schedule: { Shows: { Show: any[] } } };
+    }) as { Schedule: { Shows: { Show: any[] } } }
 
-    const shows = z.array(FinnkinoShow).parse(parsedXml.Schedule.Shows.Show);
-    return shows;
+    const shows = z.array(FinnkinoShow).parse(parsedXml.Schedule.Shows.Show)
+    return shows
 }
 
 export function toShow() {}
