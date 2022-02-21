89d24622c2c7ff9337b326003e468808bbb2c93d
Jari Mäkelä <jari.makela@albertineapp.com>
2022-02-21 20:51:59 +0200

    Parse Finnkino show list

---
+++ src/finnkino.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-underscore-dangle */
+import { xml2js } from "xml-js";
+import * as z from "zod";
+
+const TextNode = z
+    .object({ _text: z.string() })
+    .transform((node) => node._text);
+const NumberNode = z
+    .object({ _text: z.number() })
+    .transform((node) => node._text);
+const DateNode = z
+    .object({ _text: z.string().transform((s) => new Date(s)) })
+    .transform((node) => node._text);
+
+const FinnkinoShow = z.object({
+    ID: NumberNode,
+    dttmShowStartUTC: DateNode,
+    Title: TextNode,
+    OriginalTitle: TextNode,
+    ProductionYear: NumberNode,
+    LengthInMinutes: NumberNode,
+    TheatreID: NumberNode,
+    TheatreAuditriumID: NumberNode, // sic.
+    Theatre: TextNode,
+    TheatreAuditorium: TextNode,
+    PresentationMethodAndLanguage: TextNode,
+    ShowURL: TextNode,
+    EventURL: TextNode,
+});
+
+type FinnkinoShow = z.infer<typeof FinnkinoShow>;
+
+export function parseFinnkino(xml: string): FinnkinoShow[] {
+    const parsedXml = xml2js(xml, {
+        compact: true,
+        nativeType: true,
+        trim: true,
+        ignoreAttributes: true,
+        ignoreDeclaration: true,
+        ignoreDoctype: true,
+    }) as { Schedule: { Shows: { Show: any[] } } };
+
+    const shows = z.array(FinnkinoShow).parse(parsedXml.Schedule.Shows.Show);
+    return shows;
+}
+
+export function toShow() {}
