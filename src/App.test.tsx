65c984d9ee5250fcbc50dec33daf32266d7d55d8
Jari Mäkelä <jari.makela@albertineapp.com>
2022-02-27 15:47:29 +0200

    List shows

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react"
 import { render, screen } from "@testing-library/react"
+import React from "react"
 import App from "./App"
 
-test("renders learn react link", () => {
+test("renders app", () => {
     render(<App />)
-    const linkElement = screen.getByText(/learn react/i)
-    expect(linkElement).toBeInTheDocument()
+    const title = screen.getByText(/Shows/i)
+    expect(title).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from "react"
-import logo from "./logo.svg"
+import React, { useCallback, useEffect, useState } from "react"
 import "./App.css"
+import { parseFinnkino, toShow } from "./finnkino"
+import { kinotShowToShow, parseKinotJson } from "./kinot-fi"
+import { Show } from "./types"
+
+async function getKinotShows(): Promise<Show[]> {
+    const response = await fetch(
+        "https://www.kinot.fi/wp-json/kinot-shows/v1/shows?limit=3&offset=0&theater=all&date=all"
+    )
+    const kinotResponse = await response.json()
+    const kinotResult = parseKinotJson(kinotResponse)
+    return kinotResult.map(kinotShowToShow)
+}
+
+async function getFinnkinoShows(): Promise<Show[]> {
+    const response = await fetch(
+        "https://www.finnkino.fi/xml/Schedule?area=1002"
+    )
+    const finnkinoXml = await response.text()
+    const finnkinoShows = parseFinnkino(finnkinoXml)
+    return finnkinoShows.map(toShow)
+}
 
 function App() {
+    const [shows, showsSet] = useState<Show[]>([])
+
+    const getShows = useCallback(async () => {
+        const kinotShows = await getKinotShows()
+        const finnkinoShows = await getFinnkinoShows()
+        const allShows = kinotShows.concat(finnkinoShows)
+        showsSet(allShows)
+    }, [])
+
+    useEffect(() => {
+        getShows()
+    }, [getShows])
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <div>
+                <h1>Shows</h1>
+                {shows.map((s) => (
+                    <div key={`${s.operator}-${s.operatorId}`}>
+                        <h2>{s.movie.localTitles[0].value}</h2>
+                        <ul>
+                            <li>{s.startTime.toISOString()}</li>
+                            <li>{s.theatre}</li>
+                        </ul>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
