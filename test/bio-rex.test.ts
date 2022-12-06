892726f3c937c551f5b9d981bc6a3d4624ca4cdd
Jari Mäkelä <jari@iki.fi>
2022-12-06 10:44:30 +0200

    Add tests for Bio Rex parsing

---
+++ test/bio-rex.test.ts
@@ -0,0 +1,27 @@
+import { expect } from "chai"
+import { readFileSync } from "fs"
+import { bioRexJsonToDom } from "../src/bio-rex"
+
+describe("Bio Rex", () => {
+    it("can convert html to shows", () => {
+        const json = JSON.parse(
+            readFileSync("./test-data/bio-rex.json").toString()
+        )
+
+        const shows = bioRexJsonToDom(json)
+        const firstShow = shows[0]
+        expect(firstShow.startTime.toISOString()).equal(
+            "2022-12-06T10:15:00.000Z"
+        )
+        expect(firstShow.title).to.equal("Violent Night")
+        expect(firstShow.theatre).to.equal("BioRex Redi")
+        expect(firstShow.screen).to.equal("Sali 1")
+        expect(firstShow.url).to.equal(
+            "https://biorex.fi/elokuvat/violent-night/?f_cinemas=all"
+        )
+        expect(firstShow.operator).to.equal("BioRex")
+        expect(firstShow.operatorId).to.equal(
+            "https://biorex.fi/elokuvat/violent-night/"
+        )
+    })
+})
